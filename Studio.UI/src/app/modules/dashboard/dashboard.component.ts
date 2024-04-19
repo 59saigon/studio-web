80da0346a25f0be4d13f0e2fd1356eabeea34c99
sonnh1106 <nguyenson20zzzz@gmail.com>
2024-04-19 11:35:40 +0700

    fix UI app dashboard

--- Studio.UI/src/app/modules/dashboard/dashboard.component.ts
+++ Studio.UI/src/app/modules/dashboard/dashboard.component.ts
@@ -7,13 +7,17 @@ import { SidebarService } from 'src/app/data/services/sidebar';
 import { SidebarComponent } from "../../data/services/common/sidebar.component";
 import { SidebarItemComponent } from "../../data/services/common/sidebar-item.component";
 import { SidebarItemGroupComponent } from "../../data/services/common/sidebar-item-group.component";
+import { CommonModule } from '@angular/common';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-dashboard',
     standalone: true,
     templateUrl: './dashboard.component.html',
     styleUrls: ['./dashboard.component.scss'],
-    imports: [DarkThemeToggleComponent, NavbarComponent, SidebarComponent, SidebarItemComponent, SidebarItemGroupComponent]
+    imports: [
+      CommonModule, RouterOutlet, RouterModule, 
+      DarkThemeToggleComponent, NavbarComponent, SidebarComponent, SidebarItemComponent, SidebarItemGroupComponent]
 })
 export class DashboardComponent implements OnInit {
   title = 'dashboard';
